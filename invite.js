4fa74dec584f39809976a9f4dc894903d92af913
sd12354 <dbosdesigns432@gmail.com>
2024-12-14 16:18:56 -0500

    e

--- invite.js
+++ invite.js
@@ -9,8 +9,8 @@ var x = setInterval(function() {
   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown").innerHTML = days + " Days " + hours + " Hours "
+  + minutes + " Minutes " + seconds + " Seconds ";
 
   if (distance < 0) {
     clearInterval(x);
